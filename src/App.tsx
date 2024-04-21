ffbddd7079dd77b330afeee3ff37c49e2bfa52e3
Rahul Kandikatla <rahulkandikatla33@gmail.com>
2024-04-21 21:44:09 +0530

    add audio recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import Header from "./components/Header";
-import RecordingButton from "./components/RecordingButton";
+import "./App.css";
+import { useState } from "react";
+import AudioRecorder from "./components/AudioRecorder";
 
-function App() {
-  return (
-    <div className="w-96">
-      <Header />
-      <RecordingButton />
-    </div>
-  );
-}
-
+const App = () => {
+    let [recordOption, setRecordOption] = useState("video");
+    const toggleRecordOption = (type: any) => {
+      recordOption; // remove this line
+        return () => {
+            setRecordOption(type);
+        };
+    };
+    return (
+        <div>
+            <h1>React Media Recorder</h1>
+            <div className="justify-center items-center gap-10">
+                <button onClick={toggleRecordOption("audio")}>
+                  Record Audio
+                </button>
+            </div>
+            <div>
+                <AudioRecorder />
+            </div>
+        </div>
+    );
+};
 export default App;
