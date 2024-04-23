ff1a62787075bf8c64863d99a6d845ea7678e78c
Rahul Kandikatla <rahul.kandikatla@nxtwave.co.in>
2024-04-23 18:32:51 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import "./App.css";
-import { useState } from "react";
 import AudioRecorder from "./components/AudioRecorder";
 
 const App = () => {
-    let [recordOption, setRecordOption] = useState("video");
-    const toggleRecordOption = (type: any) => {
-      recordOption; // remove this line
-        return () => {
-            setRecordOption(type);
-        };
-    };
-    return (
-        <div>
-            <h1>React Media Recorder</h1>
-            <div className="justify-center items-center gap-10">
-                <button onClick={toggleRecordOption("audio")}>
-                  Record Audio
-                </button>
-            </div>
-            <div>
-                <AudioRecorder />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center w-full">
+      <h1 className="text-3xl font-bold">Audio Recorder</h1>
+      <div>
+        <AudioRecorder />
+      </div>
+    </div>
+  );
 };
 export default App;
